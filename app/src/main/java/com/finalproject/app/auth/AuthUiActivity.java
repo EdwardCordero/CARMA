58a0c3f8e38e1db9b6e1802536e5e548630fa1e5
MJRAMZ <34893606+MJRAMZ@users.noreply.github.com>
2020-10-18 19:52:08 -0700

    Added new user registration activity.

--- app/src/main/java/com/finalproject/app/auth/AuthUiActivity.java
+++ app/src/main/java/com/finalproject/app/auth/AuthUiActivity.java
@@ -4,38 +4,40 @@ package com.finalproject.app.auth;
 import android.content.Context;
 import android.os.Bundle;
 import android.content.Intent;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.ListView;
+import android.widget.EditText;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.finalproject.app.MainActivity;
 import com.finalproject.app.R;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class AuthUiActivity extends AppCompatActivity {
 
+    /***********************************
+     * USER REGISTRATION VARIABLES
+    *************************************/
+    // Buttons
     private Button btnRegister;
+    private Button btnLogin;
+    private Button btnResetPassword;
+
+
     /***********************************
      * FIREBASE INSTANCE VARIABLES
      ************************************/
@@ -44,6 +46,9 @@ public class AuthUiActivity extends AppCompatActivity {
     private List<AuthUI.IdpConfig> providers = Arrays.asList(
             new AuthUI.IdpConfig.EmailBuilder().build(),
             new AuthUI.IdpConfig.GoogleBuilder().build());
+
+    // Firebase Auth instance
+    private  FirebaseAuth fbAuth;
 
     @NonNull
     public static Intent createIntent(@NonNull Context context){
@@ -55,15 +60,32 @@ public class AuthUiActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.auth_ui_layout);
 
-        btnRegister = (Button) findViewById(R.id.btnSignInRegister);
+        fbAuth = FirebaseAuth.getInstance();
+
+        // Buttons
+        btnRegister = findViewById(R.id.btnRegister);
+        btnLogin = findViewById(R.id.btnSignIn);
+        btnResetPassword = findViewById(R.id.btnForgotPassword);
+
+        // New user registration page
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                handleLoginRegister();
+                Intent intent = new Intent(AuthUiActivity.this, RegistrationActivity.class);
+                startActivity(intent);
             }
         });
 
-        if (FirebaseAuth.getInstance().getCurrentUser() != null){
+        // Returning user sign-in
+       btnLogin.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               handleLoginRegister();
+           }
+       });
+
+        // Check if user is already logged in
+        if (fbAuth.getCurrentUser() != null){
             startActivity(new Intent(this, MainActivity.class));
             this.finish();
         }
@@ -98,8 +120,6 @@ public class AuthUiActivity extends AppCompatActivity {
         }
     }
 
-
-
     public void handleLoginRegister() {
         startActivityForResult(
                 AuthUI.getInstance()
@@ -112,7 +132,5 @@ public class AuthUiActivity extends AppCompatActivity {
                         .build(),
                 RC_SIGN_IN);
     }
-
-
 
 } // End AuthUiActivity class
