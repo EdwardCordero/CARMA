06636435cef8f5eab2b8baf7df00c751b2383656
MJRAMZ <34893606+MJRAMZ@users.noreply.github.com>
2020-11-11 13:56:52 -0800

    Created User class for user reference

---
+++ app/src/main/java/com/finalproject/app/db/User.java
@@ -0,0 +1,37 @@
+package com.finalproject.app.db;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+@IgnoreExtraProperties
+public class User {
+    // User data
+    String userName;
+    String userEmail;
+
+    // Default constructor
+    // Required for calls to DataSnapshot.getValue(User.class)
+    public User(){
+
+    }
+
+    // Constructor for both username and email arguments
+    public User(String uname, String email){
+        this.userName = uname;
+        this.userEmail = email;
+    }
+
+    // Constructor for just email arg
+    public User(String email){
+        this.userEmail = email;
+    }
+
+    public String getUserName(){
+        return userName;
+    }
+
+    public String getUserEmail(){
+        return userEmail;
+    }
+
+
+}
