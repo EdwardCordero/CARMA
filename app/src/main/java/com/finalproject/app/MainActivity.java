afc18e8cbaec4339cd49a43e6a0fb5e6b5b16152
MJRAMZ <34893606+MJRAMZ@users.noreply.github.com>
2020-11-18 10:35:24 -0800

    Updated login page to match new UI. Renamed AuthUIActivity to LoginActivity. Updated references in related files.

--- app/src/main/java/com/finalproject/app/MainActivity.java
+++ app/src/main/java/com/finalproject/app/MainActivity.java
@@ -1,7 +1,6 @@
 package com.finalproject.app;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,29 +8,16 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
-import android.widget.Toolbar;
 
-import com.finalproject.app.auth.AuthUiActivity;
+import com.finalproject.app.auth.LoginActivity;
 import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.IdpResponse;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.fragment.app.Fragment;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 
-import java.util.Arrays;
-import java.util.List;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -83,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void startLoginActivity(){
         // Create intent for AuthUI Login activity
-        Intent intent = new Intent(this, AuthUiActivity.class);
+        Intent intent = new Intent(this, LoginActivity.class);
         // Start the login activity
         startActivity(intent);
         finish();
